20c38262450947381cf1c1e6bfcbc846fab463c8
tibetmdk <156366096+tibetmdk@users.noreply.github.com>
2024-07-06 05:54:24 +0300

    42 calışmaları

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmidik <tmidik@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 18:41:46 by tmidik            #+#    #+#             */
+/*   Updated: 2024/07/05 22:11:03 by tmidik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i--;
+		write(1,"\n",2);
+	}
+}
+
+int main()
+{
+	ft_print_reverse_alphabet();
+}
---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmidik <tmidik@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 23:28:53 by tmidik            #+#    #+#             */
+/*   Updated: 2024/07/05 21:09:05 by tmidik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar((a / 10) + 48);
+			ft_putchar((a % 10) + 48);
+			ft_putchar(' ');
+			ft_putchar((b / 10) + 48);
+			ft_putchar((b % 10) + 48);
+			if (a != 98 || b != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+}
