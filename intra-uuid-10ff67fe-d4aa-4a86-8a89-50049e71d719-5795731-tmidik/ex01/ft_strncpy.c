555d5347ad1032cc1c2088066ee1afc5dc9cbfff
tibetmdk <156366096+tibetmdk@users.noreply.github.com>
2024-07-08 04:43:43 +0300

    Add files via upload

---
+++ intra-uuid-10ff67fe-d4aa-4a86-8a89-50049e71d719-5795731-tmidik/ex01/ft_strncpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmidik <tmidik@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 08:59:06 by tmidik            #+#    #+#             */
+/*   Updated: 2024/07/08 04:08:27 by tmidik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strncpy(char *dest,	char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
